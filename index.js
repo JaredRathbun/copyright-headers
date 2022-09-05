a191e4514b65edbd3b60c790c6ce6a1771a83706
JaredRathbun <rathbunj@merrimack.edu>
2022-09-04 22:36:14 -0400

    MiSSING SEMICOLON :(

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 try {
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
 
     const path = core.getInput('path');
